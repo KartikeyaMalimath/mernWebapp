0306aadb95022dfe1f8c2123768a7515c5b889cd
Kartikeya P. Malimath <kartikeyapm@gmail.com>
2018-09-07 15:01:42 +0530

    studentSchema phase1

--- schema/studentSchema.js
+++ schema/studentSchema.js
@@ -10,10 +10,12 @@ const studentSchema =  new Schema({
         type:String,
         required: [true, 'Name field is Required']
     },
+    // BE ,MTech etc
     stream:{
         type:String,
         required : [true, 'Stream is Required']
     },
+    // cse, ise etc
     branch:{
         type:String,
         required: [true, 'branch is required']
@@ -44,23 +46,200 @@ const studentSchema =  new Schema({
     },
 
     parentDetails : {
+        //Father details
             fatherName: {
-                type: String
+                type: String,
+                required: true
             },
             fatherOccupation:{
-                type: String
+                type: String,
+                required: true
             },
             fatherAge:{
-                type: Number
+                type: Number,
+                required: true
             },
             fatherIncome:{
+                type: Number,
+                required:true
+            },
+            fatherMobile: {
+                type: Number,
+                required:true,
+                maxlength:11
+            },
+            incomeClaim: {
+                required:true,
+                type:Boolean
+            },
+            //Mother Details
+            motherName: {
+                type: String,
+                required: true
+            },
+            motherOccupation: {
+                type: String,
+                required: true
+            },
+            motherAge: {
+                type:Number,
+                required:true
+            },
+            motherIncome : {
+                required: true,
                 type: Number
             },
-
-            motherName: {
+            motherMobile : {
+                type: Number
+            },
+            //Guardian Details
+            guardianName: {
+                type: String,
+            },
+            guardianOccupation : {
                 type: String
             },
-
+            guardianAge : {
+                type: Number
+            },
+            guardianMobile : {
+                type: Number
+            }
+    },
+    //Student personal Details
+    studentDetails : {
+        firstName: {
+            type: String,
+            required: true
+        },
+        middleName : {
+            type: String
+        },
+        lastName : {
+            type: String
+        },
+        religion : {
+            type: String,
+            required: true
+        },
+        caste: {
+            type:String,
+            required: true
+        },
+        subcaste : {
+            type: String
+        },
+        blood : {
+            type: String,
+            maxlength: 3
+        },
+        nationality : {
+            type: String,
+            required: true
+        },
+        motherTongue : {
+            type: String,
+            required: true
+        },
+        area : {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String
+        },
+        currentAddress : {
+            type: String,
+            required: true
+        },
+        currentCity : {
+            type: String,
+            required: true
+        },
+        currentState : {
+            type: String,
+            required: true
+        },
+        country : {
+            type: String,
+            required: true
+        },
+        pinCode : {
+            type : Number,
+        },
+        permAddress : {
+            type: String,
+            required: true
+        },
+        permCity : {
+            type: String,
+            required: true
+        },
+        permState : {
+            type: String,
+            required: true
+        },
+        permCountry : {
+            type: String,
+            required: true
+        },
+        permPinCode : {
+            type: Number,
+        },
+        phone: {
+            type: Number
+        },
+        emergencyContact : {
+            type: Number,
+            maxlength: 12
+        },
+        birthPlace : {
+            type: String,
+            required: true
+        },
+        birthState: {
+            type: String,
+            required: true
+        },
+        birthCountry : {
+            type: String,
+            required: true
+        },
+        disability: {
+            type: String,
+            required: true
+        },
+        disableType : {
+            type: String,
+        },
+        backward : {
+            type: String,
+            required: true
+        },
+        AdharCard: {
+            type: Number
+        },
+        siblings: {
+            type: String
+        }
+    },
+    academicHistory: {
+        schoolName: {
+            type: String,
+            required: true
+        },
+        schoolBoard: {
+            type: String,
+            required: true
+        },
+        schoolTown: {
+            type: String,
+            required: true
+        },
+        schoolState: {
+            type: String,
+            required: true
+        },
     }
 });
 
